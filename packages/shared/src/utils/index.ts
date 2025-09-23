ef22ee6f963bd344508c6aab4091a434a328ec34
fs-nathan <nathan.makeitpro@gmail.com>
2025-09-23 11:38:58 +0700

    feat(shared): add string utils

--- packages/shared/src/utils/index.ts
+++ packages/shared/src/utils/index.ts
@@ -2,3 +2,4 @@ export * from './string';
 export * from './array';
 export * from './query-string';
 export * from './date';
+export * from './string';
--- packages/shared/src/utils/string.ts
+++ packages/shared/src/utils/string.ts
@@ -1,3 +1,19 @@
 export const capitalize = (str: string): string => {
   return str.charAt(0).toUpperCase() + str.slice(1).toLowerCase();
 };
+
+export const camelToDashed = (camel: string) => {
+  return camel.replace(/[A-Z]/g, (m) => '-' + m.toLowerCase());
+};
+
+export const maskedEmail = (email?: string) => {
+  if (!email) return '';
+  const [localPart, domain] = email.split('@');
+  if (!localPart || localPart.length <= 2) return email;
+
+  const firstChar = localPart[0];
+  const lastChar = localPart[localPart.length - 1];
+  const maskedMiddle = '*'.repeat(localPart.length - 2);
+
+  return `${firstChar}${maskedMiddle}${lastChar}@${domain}`;
+};
